bbf2c16f190896b87b97c651bb7434cf91ff876f
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-12-01 14:05:02 +0530

    auth

--- halls/models.py
+++ halls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Hall(models.Model):
+    title = models.CharField(max_length=255)
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    youtube_id = models.CharField(max_length=255)
+    hall = models.ForeignKey(Hall,on_delete=models.CASCADE)
+    
