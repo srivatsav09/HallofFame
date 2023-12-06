95b12b2cc6ea3f74e521e2c6d5101a561e441e9c
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-12-06 22:11:41 +0530

    auth: user logs in directly after signup

--- halls/views.py
+++ halls/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Hall
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
@@ -14,6 +17,15 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+    # we can make the user login directly after signup
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get(
+            'username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 
 class CreateHall(generic.CreateView):
@@ -21,6 +33,11 @@ class CreateHall(generic.CreateView):
     fields = ['title']
     template_name = 'halls/create_hall.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
 
 # use cbv if ur using a particular base of  a model which is not too complicated
 # like for an object if we want to create a view to create,update delete then use cbv
