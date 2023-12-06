292d1657d38c9efd9d235a8ea8c769190343a8fa
Srivatsav <srivatsav.vijay2003@gmail.com>
2023-12-06 20:05:35 +0530

    create hall cbv created

--- HallofFame/urls.py
+++ HallofFame/urls.py
@@ -29,5 +29,8 @@ urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Halls
+    path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- halls/views.py
+++ halls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Hall
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
@@ -13,3 +14,14 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
+
+# use cbv if ur using a particular base of  a model which is not too complicated
+# like for an object if we want to create a view to create,update delete then use cbv
+# for The model Video it might get a bit messy using only cbvs then we use function based views as well
